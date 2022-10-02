6fff390159f5bf474d2ec1fdb94ef3703c9f1413
Utsab Paroi <utsab@Utsabs-MacBook-Pro.local>
2022-10-02 22:42:13 +0600

    Initial setup

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+    @include('layouts.partial._head')
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+    @include('layouts.partial._navbar')
+    @include('layouts.partial._sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+    @include('layouts.partial._footer')
+</div>
+<!-- ./wrapper -->
+
+@include('layouts.partial._footer-script')
+</body>
+</html>
